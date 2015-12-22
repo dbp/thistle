c4a0c78868c838f0b330d92e305eeb9f070cc378
Daniel Patterson <dbp@dbpmail.net>
2015-12-22 15:53:16 -0500

    add some basic eval cases

--- src/Lang.hs
+++ src/Lang.hs
@@ -1,7 +1,8 @@
 module Lang where
 
-import qualified Data.Map  as M
-import           Data.Text (Text)
+import qualified Data.Map    as M
+import           Data.Monoid ((<>))
+import           Data.Text   (Text)
 
 data T = TInt
        | TDouble
@@ -12,7 +13,7 @@ data T = TInt
        | TLam [T] T
        deriving (Show, Eq)
 
-data Var = Var Text deriving (Show, Eq)
+data Var = Var Text deriving (Show, Eq, Ord)
 
 data Prim = PAdd deriving (Show, Eq)
 
@@ -37,17 +38,34 @@ data ES = ES Id T deriving (Show, Eq)
 
 data Env = Env (M.Map Var V) deriving (Show, Eq)
 
+emptyEnv :: Env
+emptyEnv = Env M.empty
+
+envFromList :: [(Var, V)] -> Env
+envFromList ls = Env (M.fromList ls)
+
+extendEnv :: Env -> [(Var, V)] -> Env
+extendEnv (Env e) vs = Env (M.union (M.fromList vs) e)
+
+data TEnv = TEnv (M.Map Var T) deriving (Show, Eq)
+
+emptyTEnv :: TEnv
+emptyTEnv = TEnv M.empty
+
 data V = VInt Int
        | VDouble Double
        | VBool Bool
        | VString Text
-       | VList [VS]
-       | VObject (M.Map Text VS)
+       | VList [V]
+       | VObject (M.Map Text V)
        | VLam Env [Var] E
        | VSource VS
        deriving (Show, Eq)
 
-data VS = VS Id [UserId] V deriving (Show, Eq)
+data VS = VSBase Id [UserId] V
+        | VSList Id [UserId] [VS]
+        | VSObject Id [UserId] (M.Map Text VS)
+        deriving (Show, Eq)
 
 data Id = Id Int deriving (Show, Eq)
 
@@ -55,8 +73,34 @@ data UserId = UserId Int deriving (Show, Eq)
 
 
 
-tc :: E -> T
+tc :: TEnv -> E -> T
 tc = undefined
 
-eval :: E -> (V, [VS])
-eval = undefined
+eval :: Env -> E -> (V, [VS])
+eval _env (EInt n)         = (VInt n, [])
+eval _env (EDouble d)      = (VDouble d, [])
+eval _env (EBool b)        = (VBool b, [])
+eval _env (EString t)      = (VString t, [])
+eval env (EList es)        = let vs = map (eval env) es
+                             in (VList (map fst vs), concatMap snd vs)
+eval env (EObject fs)      = undefined
+eval (Env env) (EVar v)    =
+  case M.lookup v env of
+    Nothing -> error $ "Could not find " <> show v <> " in env " <> show env
+    Just v -> (v, [])
+eval env (ELam vs e)       = (VLam env vs e, [])
+eval env (EApp e es)       =
+  case eval env e of
+    (lam@(VLam venv vs body), sources) ->
+      if length vs == length es
+         then let args = map (eval env) es
+                  res = eval (extendEnv venv (zip vs (map fst args))) body
+              in (fst res, snd res <> concatMap snd args <> sources)
+         else error $ "Wrong number of arguments to lambda " <> show lam <> ". Expected " <> show vs <> ", got" <> show es <> "."
+    l -> error $ "Applying a non-lambda " <> show l <> " to arguments " <> show es
+eval env (EIf c t e)       = undefined
+eval env (ECase e n h t s) = undefined
+eval env (EDot e f)        = undefined
+eval env (ELet v e)        = undefined
+eval env (EPrim p es)      = undefined
+eval env (ESource es)      = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import           Test.Hspec
 
 import           Lang
 
 main :: IO ()
-main = hspec $ do it "should eval constants" $ eval (EInt 10) `shouldBe` (VInt 10, [])
+main = hspec $ do
+  describe "constants" $ do
+    it "ints" $
+      eval emptyEnv (EInt 10) `shouldBe` (VInt 10, [])
+    it "doubles" $
+      eval emptyEnv (EDouble 10) `shouldBe` (VDouble 10, [])
+    it "strings" $
+      eval emptyEnv (EString "hello") `shouldBe` (VString "hello", [])
+    it "bools" $
+      eval emptyEnv (EBool True) `shouldBe` (VBool True, [])
+    it "lists" $
+      eval emptyEnv (EList [EInt 1, EInt 2])
+         `shouldBe` (VList [VInt 1, VInt 2], [])
+  describe "vars, lams, and application" $ do
+    it "lookup" $
+      eval (envFromList [(Var "x", VInt 10)]) (EVar (Var "x"))
+        `shouldBe` (VInt 10, [])
+    it "applying one argument functions" $
+      eval emptyEnv (EApp (ELam [Var "x"] (EVar (Var "x"))) [EInt 10])
+           `shouldBe` (VInt 10, [])
